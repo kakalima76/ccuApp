d937c504ad5bbab574e2eee8af53440a704bca25
kakalima76 <nieraldo@gmail.com>
2019-07-15 00:47:12 -0300

    heroku 4 upload

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+var favicon = require('serve-favicon');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/app'));
+app.use(favicon(path.join(__dirname, 'src', 'favicon.ico')));
 
 app.get('/*', function(req,res) {
     
